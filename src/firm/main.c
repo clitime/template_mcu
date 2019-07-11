48146372f210b7b1e7e896899f4e578a6d57072e
smirnov evgeniy <smirnov-eas@yandex.com>
2019-07-11 22:35:20 +0300

    new(): add inout driver

--- src/firm/main.c
+++ src/firm/main.c
@@ -1,10 +1,13 @@
 #include "stm32f4xx.h"
-
+#include "inout.h"
 
 int main(void) __attribute__((noreturn));
 
 
 int main(void) {
+    configInput();
+    configOutput();
+
     while (1) {
         continue;
     }
